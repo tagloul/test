8171af5e8e6d7b80d14e590adc5102ad45837cf9
tagloul <ynemmour@student.ethz.ch>
2014-03-31 04:17:28 +0200

    added the SBAClass mentioned in the last commit

---
+++ SBAClass.py
@@ -0,0 +1,93 @@
+#==============================================================================
+# Scalable Broadcast Algortihm
+#==============================================================================
+import random
+import math
+import NodeClass as nde
+import networkx as nx
+
+
+class SBA(nde.Node):
+
+    def __init__(self, size, iteration, graph):
+        super(SBA, self).__init__(size, iteration)
+        self.two_hop_neigh = []
+        # contains all messages currently processed by SBA
+        self.packet_dict = {}
+        self.cover_dict = {}
+
+    def update_packet_dict(self, actual_iter, graph):
+        packets_to_del = []
+        for packet in self.packet_dict:
+            t, start_iter = self.packet_dict[packet]
+            print('actual_iter :', actual_iter)
+            print('limit_iter :', start_iter+t)
+            if (start_iter + t) <= actual_iter:
+                #===============================================================
+                # del self.packet_dict[packet]
+                #===============================================================
+                packets_to_del.append(packet)
+                for node in graph.neighbors_iter(self):
+                    bool_cs = node in self.cover_dict[packet]
+                    print(bool_cs)
+                    if bool_cs == False:
+                        break
+                if bool_cs == False:
+                    node.sending_buffer.append(packet)
+            else:
+                self.update_cover_set(packet, graph)
+        for pack in packets_to_del:
+            del self.packet_dict[pack]
+
+    def check_receive_buffer(self, m, actual_iter, graph):
+        bool_ds = m in self.data_stack
+        print(self.data_stack)
+        print(m)
+        bool_pd = m in self.packet_dict
+        print('bool_ds', bool_ds)
+        print('bool_pd', bool_pd)
+        if bool_pd == True and bool_ds == True:
+            self.update_coverset(m, graph)
+        elif bool_pd == False and bool_ds == False:
+            #self.data_stack.append(m)
+            bool_neigh = self.check_neigh(graph, m.last_node)
+            if bool_neigh == True:
+                pass
+            elif bool_neigh == False:
+                t = 0
+                #t = self.get_random_timer(graph)
+                print('timer')
+                print(t)
+                self.packet_dict[m] = (t, actual_iter)
+                self.update_cover_set(m, graph)
+
+    def check_neigh(self, graph, neigh):
+        for node in graph.neighbors_iter(self):
+            bool_value = node in graph.neighbors(neigh) or node == neigh
+            if bool == False:
+                break
+        return bool_value
+
+    def get_random_timer(self, graph):
+        # calculates the quotient T_0 of the 1 + max degree of neighbors
+        # and 1 + degree of message receiver
+        degree_neigh = 0
+        for neigh in graph.neighbors_iter(self):
+            if graph.degree(neigh) > degree_neigh:
+                degree_neigh = graph.degree(neigh)
+        degree_node = graph.degree(self)
+        T_0 = float(1 + degree_neigh) / (1 + degree_node)
+        # set the random time value for the argument of the
+        # uniform-distribution
+        random_timer = 2
+        t = random.randint(0, math.ceil(T_0 * random_timer))
+        return t
+
+    def update_cover_set(self, m, graph):
+        if m not in self.cover_dict:
+            self.cover_dict[m] = []
+        if m.last_node in self.cover_dict[m] == False:
+            self.cover_dict[m].append(m.last_node)
+        for neigh in graph.neighbors_iter(m.last_node):
+            if neigh not in self.cover_dict[m]:
+                self.cover_dict[m].append(neigh)
